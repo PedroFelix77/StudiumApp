94649fa61c5483010de234381dbbb524c11e6da8
Pedro Henrique Felix Cavalcanti <pedrohfcavalcanti7@gmail.com>
2025-11-10 00:25:00 -0300

    feat: adiciona repositório para instituições com método de busca

--- studium-academico/src/main/java/com/studium/studium_academico/infrastructure/repository/InstitutionRepository.java
+++ studium-academico/src/main/java/com/studium/studium_academico/infrastructure/repository/InstitutionRepository.java
@@ -4,8 +4,10 @@ import com.studium.studium_academico.infrastructure.entity.Institution;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface InstitutionRepository extends JpaRepository<Institution, UUID> {
+    Optional<Institution> findFirstByOrderByIdAsc();
 }
