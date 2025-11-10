676e0a8724f7979d75ed506922e427b3bce9d1aa
Pedro Henrique Felix Cavalcanti <pedrohfcavalcanti7@gmail.com>
2025-11-10 00:24:12 -0300

    feat: implementa autenticação JWT com SecurityConfig, SecurityFilter e TokenService

--- studium-academico/src/main/java/com/studium/studium_academico/infrastructure/security/SecurityConfig.java
+++ studium-academico/src/main/java/com/studium/studium_academico/infrastructure/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.studium.studium_academico.infrastructure.security;
 
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,11 +15,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+    @Autowired
+    SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -68,6 +73,7 @@ public class SecurityConfig {
                         // Qualquer outra rota precisa de autenticação
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
---
+++ studium-academico/src/main/java/com/studium/studium_academico/infrastructure/security/TokenService.java
@@ -0,0 +1,50 @@
+package com.studium.studium_academico.infrastructure.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.studium.studium_academico.infrastructure.entity.Users;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+    public String generateToken(Users users) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create().withIssuer("studium").
+                    withSubject(users.getEmail())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+            return token;
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Error while creating JWT",  exception);
+        }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("studium")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception){
+            return "";
+        }
+    }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
