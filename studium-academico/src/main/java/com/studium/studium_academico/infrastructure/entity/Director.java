860fd466327630bc4d87da975e863476970fde71
Pedro Henrique Felix Cavalcanti <pedrohfcavalcanti7@gmail.com>
2025-11-23 21:11:26 -0300

    feat: implementa gestão de diretores

--- studium-academico/src/main/java/com/studium/studium_academico/infrastructure/entity/Director.java
+++ studium-academico/src/main/java/com/studium/studium_academico/infrastructure/entity/Director.java
@@ -6,6 +6,8 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "director")
 @Getter
@@ -17,4 +19,7 @@ public class Director extends BaseEntity {
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private Users user;
+
+    private LocalDate hireDate;
+
 }
