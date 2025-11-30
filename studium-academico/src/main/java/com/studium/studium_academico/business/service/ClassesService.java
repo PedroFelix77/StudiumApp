728f2030c92ed42ef9feeb4777c027e8c8968e48
Pedro Henrique Felix Cavalcanti <pedrohfcavalcanti7@gmail.com>
2025-11-30 14:48:29 -0300

    feat: adiciona serviços de Classes e Classroom

---
+++ studium-academico/src/main/java/com/studium/studium_academico/business/service/ClassesService.java
@@ -0,0 +1,8 @@
+package com.studium.studium_academico.business.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassesService {
+
+}
---
+++ studium-academico/src/main/java/com/studium/studium_academico/business/service/ClassroomService.java
@@ -0,0 +1,9 @@
+package com.studium.studium_academico.business.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassroomService {
+
+
+}
