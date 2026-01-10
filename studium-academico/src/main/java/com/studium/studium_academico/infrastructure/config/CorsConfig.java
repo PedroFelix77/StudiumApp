621985292e84ed8171a7796ab4e09af95faacfad
Pedro Henrique Felix Cavalcanti <pedrohfcavalcanti7@gmail.com>
2026-01-10 00:23:40 -0300

    ajuste cors

--- studium-academico/src/main/java/com/studium/studium_academico/infrastructure/config/CorsConfig.java
+++ studium-academico/src/main/java/com/studium/studium_academico/infrastructure/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.studium.studium_academico.infrastructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,10 +10,14 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:5173"); // seu Vite
+
+        config.addAllowedOrigin(frontendUrl);
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
